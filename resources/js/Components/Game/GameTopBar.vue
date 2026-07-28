7898bb4383f90660cc2c436ff3b87164d1a9d0ad
PJSoftwareDeveloper <koksu030@gmail.com>
2026-07-28 22:54:46 +0200

    fix: header nav buttons

--- resources/js/Components/Game/GameTopBar.vue
+++ resources/js/Components/Game/GameTopBar.vue
@@ -33,11 +33,12 @@ function logout(): void {
         </div>
 
         <nav id="top-nav">
-            <button class="nav-btn" type="button" disabled>FORUM</button>
+            <button class="nav-btn" type="button" :style="{ width: '62px', height: '42px', backgroundPositionX: '-539px'}" disabled>FORUM</button>
             <button
                 class="nav-btn"
                 :class="{ active: props.active === 'rankings' }"
                 type="button"
+                 :style="{ width: '81px', height: '42px', backgroundPositionX: '-601px'}"
                 @click="visit('/rankings')"
             >
                 RANKINGI
@@ -46,12 +47,13 @@ function logout(): void {
                 class="nav-btn"
                 :class="{ active: props.active === 'achievements' }"
                 type="button"
+                 :style="{ width: '104px', height: '42px', backgroundPositionX: '-682px'}"
                 @click="visit('/achievements')"
             >
                 OSIĄGNIĘCIA
             </button>
-            <button class="nav-btn" type="button" @click="emit('settings')">KONFIGURACJA</button>
-            <button class="nav-btn logout" type="button" @click="logout">WYLOGUJ</button>
+            <button class="nav-btn" type="button" :style="{ width: '116px', height: '42px', backgroundPositionX: '-786px'}" @click="emit('settings')">KONFIGURACJA</button>
+            <button class="nav-btn logout" type="button" :style="{ width: '78px', height: '42px', backgroundPositionX: '-902px'}" @click="logout">WYLOGUJ</button>
         </nav>
     </header>
 </template>
